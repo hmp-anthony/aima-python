536bc90842bb9833df2b186e78ee5d6c04c57c1a
hmp-anthony <anthonygoddard962@gmail.com>
2024-09-13 13:15:34 +0100

    routine update

--- search_2.py
+++ search_2.py
@@ -270,7 +270,91 @@ class RouteProblem(Problem):
         "Straight-line distance between state and the goal."
         locs = self.map.locations
         return straight_line_distance(locs[node.state], locs[self.goal])
+
+
+
+class EightPuzzle(Problem):
+    """ The problem of sliding tiles numbered from 1 to 8 on a 3x3 board,
+    where one of the squares is a blank, trying to reach a goal configuration.
+    A board state is represented as a tuple of length 9, where the element at index i 
+    represents the tile number at index i, or 0 if for the empty square, e.g. the goal:
+        1 2 3
+        4 5 6 ==> (1, 2, 3, 4, 5, 6, 7, 8, 0)
+        7 8 _
+    """
+
+    def __init__(self, initial, goal=(0, 1, 2, 3, 4, 5, 6, 7, 8)):
+        assert inversions(initial) % 2 == inversions(goal) % 2 # Parity check
+        self.initial, self.goal = initial, goal
     
+    def actions(self, state):
+        """The indexes of the squares that the blank can move to."""
+        moves = ((1, 3),    (0, 2, 4),    (1, 5),
+                 (0, 4, 6), (1, 3, 5, 7), (2, 4, 8),
+                 (3, 7),    (4, 6, 8),    (7, 5))
+        blank = state.index(0)
+        return moves[blank]
+    
+    def result(self, state, action):
+        """Swap the blank with the square numbered `action`."""
+        s = list(state)
+        blank = state.index(0)
+        s[action], s[blank] = s[blank], s[action]
+        return tuple(s)
+    
+    def h1(self, node):
+        """The misplaced tiles heuristic."""
+        return hamming_distance(node.state, self.goal)
+    
+    def h2(self, node):
+        """The Manhattan heuristic."""
+        X = (0, 1, 2, 0, 1, 2, 0, 1, 2)
+        Y = (0, 0, 0, 1, 1, 1, 2, 2, 2)
+        return sum(abs(X[s] - X[g]) + abs(Y[s] - Y[g])
+                   for (s, g) in zip(node.state, self.goal) if s != 0)
+    
+    def h(self, node): return h2(self, node)
+    
+    
+def hamming_distance(A, B):
+    "Number of positions where vectors A and B are different."
+    return sum(a != b for a, b in zip(A, B))
+    
+
+def inversions(board):
+    "The number of times a piece is a smaller number than a following piece."
+    return sum((a > b and a != 0 and b != 0) for (a, b) in combinations(board, 2))
+    
+    
+def board8(board, fmt=(3 * '{} {} {}\n')):
+    "A string representing an 8-puzzle board"
+    return fmt.format(*board).replace('0', '_')
+
+class Board(defaultdict):
+    empty = '.'
+    off = '#'
+    def __init__(self, board=None, width=8, height=8, to_move=None, **kwds):
+        if board is not None:
+            self.update(board)
+            self.width, self.height = (board.width, board.height) 
+        else:
+            self.width, self.height = (width, height)
+        self.to_move = to_move
+
+    def __missing__(self, key):
+        x, y = key
+        if x < 0 or x >= self.width or y < 0 or y >= self.height:
+            return self.off
+        else:
+            return self.empty
+        
+    def __repr__(self):
+        def row(y): return ' '.join(self[x, y] for x in range(self.width))
+        return '\n'.join(row(y) for y in range(self.height))
+            
+    def __hash__(self): 
+        return hash(tuple(sorted(self.items()))) + hash(self.to_move)
+
     
 def straight_line_distance(A, B):
     "Straight-line distance between two points."

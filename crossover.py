49a34d7b3ddc5ed489aaff1133c467857e8cfab3
hmp-anthony <anthonygoddard962@gmail.com>
2024-10-23 12:53:58 +0100

    finished and tested crossover

--- crossover.py
+++ crossover.py
@@ -1,13 +1,10 @@
 import random 
 
-A = [1, 2, 3, 4, 5, 6, 7, 8]
-B = [2, 4, 6, 8, 7, 5, 3, 1]
 
 def cycle_crossover(A, B):
     n = len(A)
     child = [None] * n
     case = random.choice([0,1])
-    case = 0
     if(case == 0):
         # the first cycle
         child[0] = A[0]
@@ -33,9 +30,33 @@ def cycle_crossover(A, B):
 
         print(child)
 
+    if(case == 1):
+        # the first cycle
+        child[0] = B[0]
+        cycle = [B[0]]
+        n = A.index(B[0])
+        child[n] = B[n]
+        cycle.append(B[n])
+        while(cycle[-1] != cycle[0]):
+            n = A.index(cycle[-1])
+            child[n] = B[n]
+            cycle.append(B[n])
+
+        i = child.index(None)
+        child[i] = A[i]
+        cycle = [A[i]]
+        n = B.index(A[i])
+        child[n] = A[n]
+        cycle.append(A[n])
+        while(cycle[-1] != cycle[0]):
+            n = B.index(cycle[-1])
+            child[n] = A[n]
+            cycle.append(A[n])
         
+        print(child)
 
-
+A = [1, 2, 3, 4, 5, 6, 7, 8]
+B = [2, 4, 6, 8, 7, 5, 3, 1]
 cycle_crossover(A, B)
 
 
